da6b28d4c08a2834cb8f2b531784704db7adbcd8
Shashank-gangwar <gangwar22cse@gmail.com>
2024-08-20 16:43:47 +0530

    updated deployed postgresql url

--- server/question_service/database.py
+++ server/question_service/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:shashank@localhost:5432/Quizz'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:shashank@localhost:5432/Quizz'
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://quizz_database_user:dEm5ot1RhQpM30LvO9xxQFJufPlmLmq1@dpg-cr27in3qf0us739o7560-a/quizz_database'
 
 #creating a connection to database
 engine = create_engine(SQLALCHEMY_DATABASE_URL) 
