7c6406fe7cbbbde38a8f90ec8b19baec88411ef4
Shashank-gangwar <gangwar22cse@gmail.com>
2024-09-05 10:55:45 +0530

    api file added

---
+++ client/src/APIs/api.js
@@ -0,0 +1,38 @@
+export const Login_url = "http://127.0.0.1:8000/auth/login";
+
+export const Register_url = "http://127.0.0.1:8000/auth/register";
+
+export const loginUsingToken_Url = "http://127.0.0.1:8000/auth/login_token";
+
+export const refreshAccessToken_Url =
+  "http://127.0.0.1:8000/auth/refresh_access_token";
+
+export const AllQuizes_url = "http://127.0.0.1:8004/quiz";
+
+export const globalLeaderboard_url =
+  "http://127.0.0.1:8008/leaderboard/global/";
+
+export const updateUsername_Url = "http://localhost:8000/user/update_username/";
+
+export const updateEmail_Url = "http://localhost:8000/user/update_email/";
+
+export const updatePassword_Url = "http://localhost:8000/user/update_password";
+
+export const getMyQuizzes_Url = "http://127.0.0.1:8004/quiz/my_quizes";
+
+export const myAttmeptedQuiz_Url =
+  "http://127.0.0.1:8006/quiz_attempt/attempted_quizzes";
+
+export const createNewQuiz_Url = "http://127.0.0.1:8004/quiz/create_quiz";
+
+export const fiterQuizzes_Url = "http://127.0.0.1:8004/quiz/filter_quiz/";
+
+export const logout_Url = "http://localhost:8000/auth/logout";
+
+export const getQuizById_Url = "http://127.0.0.1:8004/quiz/";
+
+export const startQuiz_Url =
+  "http://127.0.0.1:8006/quiz_attempt/start_quiz?quiz_id=";
+
+export const endQuiz_Url =
+  "http://127.0.0.1:8006/quiz_attempt/complete_quiz?quiz_attempt_id=";
