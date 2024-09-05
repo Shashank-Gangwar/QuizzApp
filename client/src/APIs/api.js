e0534e71d1eb610029b8bc8fb2b484704daf475d
Shashank-gangwar <gangwar22cse@gmail.com>
2024-09-05 11:52:56 +0530

    api file url configured

--- client/src/APIs/api.js
+++ client/src/APIs/api.js
@@ -1,38 +1,49 @@
-export const Login_url = "http://127.0.0.1:8000/auth/login";
+export const Login_url =
+  "https://quizzapp-user-service.onrender.com/auth/login";
 
-export const Register_url = "http://127.0.0.1:8000/auth/register";
+export const Register_url =
+  "https://quizzapp-user-service.onrender.com/auth/register";
 
-export const loginUsingToken_Url = "http://127.0.0.1:8000/auth/login_token";
+export const loginUsingToken_Url =
+  "https://quizzapp-user-service.onrender.com/auth/login_token";
 
 export const refreshAccessToken_Url =
-  "http://127.0.0.1:8000/auth/refresh_access_token";
+  "https://quizzapp-user-service.onrender.com/auth/refresh_access_token";
 
-export const AllQuizes_url = "http://127.0.0.1:8004/quiz";
+export const AllQuizes_url = "https://quizzapp-quiz-service.onrender.com/quiz";
 
 export const globalLeaderboard_url =
-  "http://127.0.0.1:8008/leaderboard/global/";
+  "https://quizzapp-leaderboard-service.onrender.com/leaderboard/global/";
 
-export const updateUsername_Url = "http://localhost:8000/user/update_username/";
+export const updateUsername_Url =
+  "https://quizzapp-user-service.onrender.com/user/update_username/";
 
-export const updateEmail_Url = "http://localhost:8000/user/update_email/";
+export const updateEmail_Url =
+  "https://quizzapp-user-service.onrender.com/user/update_email/";
 
-export const updatePassword_Url = "http://localhost:8000/user/update_password";
+export const updatePassword_Url =
+  "https://quizzapp-user-service.onrender.com/user/update_password";
 
-export const getMyQuizzes_Url = "http://127.0.0.1:8004/quiz/my_quizes";
+export const getMyQuizzes_Url =
+  "https://quizzapp-quiz-service.onrender.com/quiz/my_quizes";
 
 export const myAttmeptedQuiz_Url =
-  "http://127.0.0.1:8006/quiz_attempt/attempted_quizzes";
+  "https://quizzapp-quiz-attempt-service.onrender.com/quiz_attempt/attempted_quizzes";
 
-export const createNewQuiz_Url = "http://127.0.0.1:8004/quiz/create_quiz";
+export const createNewQuiz_Url =
+  "https://quizzapp-quiz-service.onrender.com/quiz/create_quiz";
 
-export const fiterQuizzes_Url = "http://127.0.0.1:8004/quiz/filter_quiz/";
+export const fiterQuizzes_Url =
+  "https://quizzapp-quiz-service.onrender.com/quiz/filter_quiz/";
 
-export const logout_Url = "http://localhost:8000/auth/logout";
+export const logout_Url =
+  "https://quizzapp-user-service.onrender.com/auth/logout";
 
-export const getQuizById_Url = "http://127.0.0.1:8004/quiz/";
+export const getQuizById_Url =
+  "https://quizzapp-quiz-service.onrender.com/quiz/";
 
 export const startQuiz_Url =
-  "http://127.0.0.1:8006/quiz_attempt/start_quiz?quiz_id=";
+  "https://quizzapp-quiz-attempt-service.onrender.com/quiz_attempt/start_quiz?quiz_id=";
 
 export const endQuiz_Url =
-  "http://127.0.0.1:8006/quiz_attempt/complete_quiz?quiz_attempt_id=";
+  "https://quizzapp-quiz-attempt-service.onrender.com/quiz_attempt/complete_quiz?quiz_attempt_id=";
